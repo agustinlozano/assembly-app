653448187a12bc170b42b0195514c9cb4ea10fab
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-08 19:18:37 -0300

    Edit file: fix bug

--- newNode.s
+++ newNode.s
@@ -1,33 +1,34 @@
                 .data
-title:          .asciiz "Agregar una nueva catogoria"                
+head:           .word   0         
 getCategory: 		.asciiz "\n\tIngrese el nombre de la categoria: "
 successMessage: .asciiz "\n\tExito! categoria ingresada"
 endMessage:     .asciiz "\n\nFin del programa\n"
 buffer:         .space 16
 
+firstNodeMessage:      .asciiz "Agrega la primer categoria\n"
+newNodeMessage:        .asciiz "Agrega una nueva categoria\n"
 
         .text
 main: 
-        li $s1, 0				          #head <= null
-
-    #mostrar titulo del programa
-        la    $a0, title
-        li    $v0, 4
-        syscall
+        lw    $s1, head  		        #head <= null
 
     #hacer la invocacion de la funcion addNode
+        la    $a0, ($s1)
         jal   newNode
 
         j endApp
 
-newNode: 
+newNode:
+    #recupero head desde $a0
+        la    $t0, ($a0)
+
     #reservo memoria dinamica
         li    $v0, 9              #sbrk
         li    $a0, 16             #16bytes
         syscall
-        move  $s3, $v0            #almacena la dir. newNode
+        move  $t3, $v0            #almacena la dir. newNode
     
-    #pedir la categoria del nuevo nodo en $s3
+    #pedir la categoria del nuevo nodo en $t3
         la   $a0, getCategory     #preguntamos el nombre de la cat
         li   $v0, 4               #syscall code for printing string
         syscall
@@ -39,25 +40,29 @@ newNode:
         li   $a1, 16              #a1 ahora contiene el length
         syscall
 
-    #le mostramos al user lo que ingreso
-    #    la    $a0, buffer         #reload byte space to primary address
-    #    li    $v0, 4              #print string
-    #    syscall
-        sw    $v0, 0($s3)         #almacena la categoria en el primer campo
+    #almacena la categoria en el primer campo
+        la    $s0, buffer
+        sw    $s0, 0($t3)      #Feedback lucas
 
     #salvar retorno en la pila: apilar $ra
         addi  $sp, $sp, -4
         sw    $ra, 0($sp)
-    
-    #if head es null agregamos el primer nodo
-        bne   $s1, $0, addNewNode
-addFirstNode: 
-        move  $s1, $s3            #head ahora contiene la direccion de newnode
-        move  $s2, $s3            #tail ahora contiene la direccion de newnode
 
-        move  $a0, $s1            #paso el head como parametro
-        move  $a1, $s2            #paso el tail como parametro
-        move  $a2, $s3            #paso el newNode como parametro
+    #if head es null agregamos el primer nodo
+        bne   $t0, $0, addNewNode
+
+        la    $a0, firstNodeMessage
+        li    $v0, 4
+        syscall
+
+addFirstNode: 
+        la  $t0, ($t3)            #head ahora contiene la direccion de newnode
+        la  $t2, ($t3)            #tail ahora contiene la direccion de newnode
+
+        la  $a0, ($t0)            #paso el head como parametro
+        la  $a1, ($t2)            #paso el tail como parametro
+        la  $a2, ($t3)            #paso el newNode como parametroro
+        sw  $a2, head             #Feedback lucas
         jal   makelinksAddCategory
 
     #imprimir mensaje de exito
@@ -73,6 +78,13 @@ addFirstNode:
 
     #sino agregamos uno mas
 addNewNode:
+        la    $a0, newNodeMessage
+        li    $v0, 4
+        syscall
+
+        move  $a0, $t0            #paso el head como parametro
+        move  $a1, $t2            #paso el tail como parametro
+        move  $a2, $t3            #paso el newNode como parametro
         jal   makelinksAddCategory
 
 
@@ -84,7 +96,6 @@ makelinksAddCategory:
         move  $a1, $a2
         sw    $a0, 8($a2)
         sw    $a2, 12($a0)
-
 
         jr    $ra
 
