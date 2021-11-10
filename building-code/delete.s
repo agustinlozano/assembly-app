78d9fe6cbe331fda7d596c6b003c88d40bb51ad0
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-11-10 12:53:17 -0300

    Edit file: fixes errors but is incomplete

--- building-code/delete.s
+++ building-code/delete.s
@@ -1,13 +1,15 @@
 #deleteNode
-theOneNode:             .asciiz "\n\tSe elemininara el unico elemento existente\n\n"
-successDeleteMessage:   .asciiz "\nSe ha eliminado el nodo seleccionado\n"
-emptyObjectListMessage: .asciiz "\nLa categoria no tiene objetos ligados que borrar"
-itWillBeDeleteMessage:  .asciiz "\nSe borrara solo la categoria indicada\n\n"
+theOneNode:             .asciiz "\n\tSe elemininara el unico elemento existente\n"
+successDeleteMessage:   .asciiz "\n\tSe ha eliminado el nodo seleccionado\n"
+emptyObjectListMessage: .asciiz "\n\tLa categoria no tiene objetos ligados que borrar"
+itWillBeDeleteMessage:  .asciiz "\n\tSe borrara solo la categoria indicada"
+
 
 
 ##############################################################################
 ##                             delete node                                  ##
 ##############################################################################
+
 deleteNode:
     #recupero head y el tail
         lw    $t0, ($a0)
@@ -17,7 +19,7 @@ deleteNode:
         la    $t2, ($t0)
 
     #corroboro que la lista no este vacia
-        beq   $t2, $0, emptyList
+        beq   $t2, $0, catIsEmpty
 
     # if (current == (*head) && current == (*tail)  
         bne   $t2, $t0, normalDelete
@@ -27,17 +29,19 @@ deleteNode:
         li    $v0, 4
         syscall
 
-    #salvar retorno en la pila: apilar $ra
+    #salvar primer retorno en la pila
         addi  $sp, $sp, -4
         sw    $ra, 0($sp)
 
+        move  $a0, $t0
         jal deleteAllObjects
 
         sw    $0, head
         sw    $0, tail
 
-        #free($t2) -> como son llamado a fucines
-        #             voy a tener que gestionar $ra
+    #liberar nodo de categoria
+        move  $a0, $t2
+        jal   sfree
 
 endDeleteObj:
         la    $a0, successDeleteMessage
@@ -51,9 +55,6 @@ endDeleteObj:
         jr $ra
 
 
-
-
-
 normalDelete:
         #deleteAllObjects(*head)
         #makeLinksDeleteCategory(head, tail)
@@ -61,26 +62,35 @@ normalDelete:
         j endDeleteObj
 
 
-
-
-
 deleteAllObjects:
     # Obj *current  = NULL
         move  $t3, $0
 
-        beq   $t0, $0, emptyObjectList
-    
     # current = head -> obj;
         lw    $t4, 4($t0)
         move  $t3, $t4
+    
+        beq   $t3, $0, emptyObjectList
+
 doWhileDeletingObj:
         lw    $t3, 8($t3)
-        #free($t2) -> como son llamado a fucines
-        #             voy a tener que gestionar $ra
+    
+    #salvar segundo retorno en la pila
+        addi  $sp, $sp, -4
+        sw    $ra, 0($sp)
+
+    #liberar la memoria
+        move  $a0, $t3
+        jal   sfree
+
         bne   $t3, $t4, doWhileDeleteObj
 
         move  $t4, $0
         sw    $t4, 4($t0)
+
+    #desapilar antes de retornar
+        lw    $ra, 0($sp)
+        addi  $sp, $sp, 4
 
         jr $ra 
 
